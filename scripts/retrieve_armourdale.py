30b63f8e68bc2b2b8cc928360190f0ded33391e5
Sam Dotson <sdotson@ucsusa.org>
2024-09-03 13:52:38 -0400

    pep8 fixes

--- scripts/retrieve_armourdale.py
+++ scripts/retrieve_armourdale.py
@@ -7,11 +7,13 @@ from us import states
 
 if __name__ == "__main__":
     kc_wards_url = "https://maps.wycokck.org/gisdata/shp/ward_prec_py.zip"
-    kck_wards = gpd.read_file(kc_wards_url).to_crs(epsg=int(snakemake.config['geographic_crs']))
-    
-    armourdale_ward = '06'
-    armourdale = kck_wards[kck_wards['WARD'] == armourdale_ward].dissolve("CITY").reset_index(drop=False)
+    kck_wards = gpd.read_file(kc_wards_url).to_crs(
+        epsg=int(snakemake.config['geographic_crs']))
 
-    armourdale = armourdale[['CITY','geometry','WARD']]
-    
+    armourdale_ward = '06'
+    armourdale = kck_wards[kck_wards['WARD'] == armourdale_ward].dissolve(
+        "CITY").reset_index(drop=False)
+
+    armourdale = armourdale[['CITY', 'geometry', 'WARD']]
+
     armourdale.to_file(snakemake.output.armourdale, driver="GPKG")
